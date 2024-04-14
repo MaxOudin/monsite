496f12d067d590c0b792b2b4eedaa79a4d59a907
Maxime Oudin <oudinmaxime@gmail.com>
2024-04-14 15:33:50 +0200

    added articles management and user created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,21 @@
 class Article < ApplicationRecord
+  THEMES = [
+    "Autour du web",
+    "Les étapes clés du site internet",
+    "Les performances",
+    "Sécurité sur le web",
+    "Ecologie et développement web",
+    "Les métiers du web"
+  ]
+
   has_rich_text :content
+
+  validates :titre, presence: true, uniqueness: true
+  validates :content, presence: true
+  validates :image_url, presence: true
+  validates :image_alt, presence: true
+  validates :id, presence: true, uniqueness: true
+  validates :couleur, presence: true
+  validates :theme, presence: true, inclusion: { in: THEMES }
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,8 +25,8 @@ puts "Reset database"
 # Create user
 user = User.where(email: "oudinmaxime@gmail.com").first_or_initialize
 user.update!(
-  password: "password",
-  password_confirmation: "password"
+  password: ENV['USER_PASSWORD'],
+  password_confirmation: ENV['USER_PASSWORD']
 )
 puts "User created"
 
