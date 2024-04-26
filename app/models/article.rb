1b927934e916e1a788a82f49272ce6b3fbb0456c
Maxime Oudin <oudinmaxime@gmail.com>
2024-04-26 23:41:42 +0200

    reviewed cards with themes colored and navbar set

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,12 @@
 class Article < ApplicationRecord
-  THEMES = [
-    "Autour du web",
-    "Les étapes clés du site internet",
-    "Les performances",
-    "Sécurité sur le web",
-    "Ecologie et développement web",
-    "Les métiers du web"
-  ]
+  THEMES_WITH_COLORS = {
+    "Autour du web" => "#18435A",
+    "Les étapes clés du site internet" => "#564787",
+    "Les performances" => "#424B54",
+    "Sécurité sur le web" => "#16324F",
+    "Ecologie et développement web" => "#386641",
+    "Les métiers du web" => "2E282A"
+  }
 
   has_rich_text :content
 
@@ -15,6 +15,10 @@ class Article < ApplicationRecord
   validates :image_url, presence: true
   validates :image_alt, presence: true
   validates :couleur, presence: true
-  validates :theme, presence: true, inclusion: { in: THEMES }
+  validates :theme, presence: true, inclusion: { in: THEMES_WITH_COLORS.keys }
 
+  # Méthode pour récupérer la couleur associée à un thème
+  def couleur_du_theme
+    THEMES_WITH_COLORS[theme]
+  end
 end
