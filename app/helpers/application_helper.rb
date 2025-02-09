e61d9bd71c72f5b33fbad8a6348e0f1ab91c5d19
Maxime Oudin <oudinmaxime@gmail.com>
2025-02-09 21:08:28 +0100

    show pages improved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def rgba_with_opacity(hex_color, opacity)
+    # Supprimer le # si présent
+    hex_color = hex_color.gsub('#', '')
+
+    # Convertir les valeurs hex en RGB
+    r = hex_color[0..1].to_i(16)
+    g = hex_color[2..3].to_i(16)
+    b = hex_color[4..5].to_i(16)
+
+    # Retourner la couleur au format rgba
+    "rgba(#{r}, #{g}, #{b}, #{opacity})"
+  end
 end
