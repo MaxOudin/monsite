cd992dd62937773a909e812430d47ff15281d7ae
Maxime Oudin <oudinmaxime@gmail.com>
2025-03-14 23:42:34 +0100

    containers for mobile changed

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 
 # Épingle pour les controllers de votre application
 pin_all_from "app/javascript/controllers", under: "controllers"
