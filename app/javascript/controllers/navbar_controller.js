85cb6babdc9e074aa8e8cc58e86223365a31ab91
Maxime Oudin <oudinmaxime@gmail.com>
2025-08-31 07:11:58 +0200

    plus infos sur homepage

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,11 +3,33 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["menu"]
 
+  connect() {
+    this.handleScroll = this.handleScroll.bind(this)
+    window.addEventListener('scroll', this.handleScroll)
+    this.handleScroll() // Initial call
+  }
+
+  disconnect() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
   toggleMenu() {
     this.menuTarget.classList.toggle('active')
+    document.body.classList.toggle('menu-open')
   }
 
   closeMenu() {
     this.menuTarget.classList.remove('active')
+    document.body.classList.remove('menu-open')
+  }
+
+  handleScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    
+    if (scrollTop > 100) {
+      this.element.classList.add('scrolled')
+    } else {
+      this.element.classList.remove('scrolled')
+    }
   }
 }
