6c5048a4039622e1c985149c9d6a8b2e518cf25b
Maxime Oudin <oudinmaxime@gmail.com>
2025-12-23 22:45:58 +0100

    fichier binaire

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,18 +2,22 @@ class SitemapsController < ApplicationController
   # Le sitemap est accessible publiquement, pas besoin d'authentification
 
   def show
-    sitemap_path = Rails.root.join('public', 'sitemaps', 'sitemap.xml.gz')
+    sitemap_gz_path = Rails.root.join('public', 'sitemaps', 'sitemap.xml.gz')
     
-    if File.exist?(sitemap_path)
-      # Servir le fichier avec les bons headers pour un sitemap gzippé
-      send_file(
-        sitemap_path,
-        type: 'application/xml',
-        disposition: 'inline',
-        content_encoding: 'gzip',
-        # Headers de cache pour optimiser les performances (1 heure)
-        cache_control: 'public, max-age=3600'
-      )
+    if File.exist?(sitemap_gz_path)
+      # Pour un sitemap.xml.gz, on sert le fichier gzip avec les headers appropriés
+      # Type XML car c'est du XML compressé, avec Content-Encoding: gzip
+      # Lire le fichier binaire (déjà compressé)
+      data = File.binread(sitemap_gz_path)
+      
+      # Définir les headers appropriés pour un fichier XML gzippé
+      response.headers['Content-Type'] = 'application/xml; charset=utf-8'
+      response.headers['Content-Encoding'] = 'gzip'
+      response.headers['Content-Length'] = data.bytesize.to_s
+      response.headers['Cache-Control'] = 'public, max-age=3600'
+      
+      # Envoyer les données
+      send_data(data, disposition: 'inline')
     else
       head :not_found
     end
