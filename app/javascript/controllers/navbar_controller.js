72c25d11307732b828a75c85b37b912160ee2a33
Maxime Oudin <oudinmaxime@gmail.com>
2025-12-29 21:40:32 +0100

    tailwind basis index and show article/projet

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,11 +3,39 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["menu"]
 
+  connect() {
+    // Fermer le menu si on clique en dehors
+    document.addEventListener('click', this.handleOutsideClick.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener('click', this.handleOutsideClick.bind(this))
+  }
+
   toggleMenu() {
-    this.menuTarget.classList.toggle('active')
+    const isOpen = this.menuTarget.classList.contains('max-h-screen')
+    
+    if (isOpen) {
+      this.closeMenu()
+    } else {
+      this.openMenu()
+    }
+  }
+
+  openMenu() {
+    this.menuTarget.classList.remove('max-h-0')
+    this.menuTarget.classList.add('max-h-screen')
   }
 
   closeMenu() {
-    this.menuTarget.classList.remove('active')
+    this.menuTarget.classList.remove('max-h-screen')
+    this.menuTarget.classList.add('max-h-0')
+  }
+
+  handleOutsideClick(event) {
+    if (this.menuTarget.classList.contains('max-h-screen') && 
+        !this.element.contains(event.target)) {
+      this.closeMenu()
+    }
   }
 }
