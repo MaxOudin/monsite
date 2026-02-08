70f3f899018c128926e0d8d1aec70963b0b0aa6b
Maxime Oudin <oudinmaxime@gmail.com>
2026-02-08 09:39:35 +0100

    build tailwind css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,9 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-
-# Add additional assets to the asset load path.
-# Ajouter le dossier builds pour que Sprockets trouve tailwind.css
 Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
 
 # Precompile additional assets.
