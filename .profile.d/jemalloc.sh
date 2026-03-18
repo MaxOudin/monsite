403de560dc40357f538cb0789e03c024f08c44c5
Maxime Oudin <oudinmaxime@gmail.com>
2026-03-18 02:00:55 +0100

    jemalloc2 and sh removed

--- .profile.d/jemalloc.sh
+++
@@ -1,6 +0,0 @@
-# Précharge jemalloc pour Ruby (réduit la fragmentation mémoire)
-# Nécessite libjemalloc dans l'Aptfile
-JEMALLOC_LIB=$(find /app/.apt/usr/lib -name 'libjemalloc.so*' 2>/dev/null | head -1)
-if [ -n "$JEMALLOC_LIB" ]; then
-  export LD_PRELOAD="$JEMALLOC_LIB"
-fi
